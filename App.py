a5c1892507ec4af3cc860179b936e16b4eab5c5e
Nico <nicolascanica@gmail.com>
2023-09-07 12:21:08 -0300

    agregando datos

---
+++ App.py
@@ -0,0 +1,17 @@
+import liquidacion as lq
+
+# Voy a definir unas listas con Nombres, Valor por hora y Faltas de empleados para poder ir probando la api
+
+nombres = ["Nicolas","Juan Cruz","Ramiro","Agustin","Charly","Pedro","Gustavo","Noelia","Catalina"]
+
+valorxhora = [3000,3200,2150,1500,4000,1000,1500,2500,2550]
+
+faltas = [2,0,1,0,0,0,3,1,0]
+
+empleados = []
+for n,v,f in zip(nombres,valorxhora,faltas):
+    empleados.append({
+                        "Nombre":n,
+                        "Valor por hora":v,
+                        "Faltas":f
+                    })
