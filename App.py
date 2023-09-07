e90720b90cc14ab2cc49dfe931d52ebe5715d287
Nico <nicolascanica@gmail.com>
2023-09-07 13:06:16 -0300

    Agregando datos para la prueba

--- App.py
+++ App.py
@@ -3,27 +3,6 @@
 import liquidacion as lq
 from typing import List
 
-# Voy a definir unas listas con Nombres, Valor por hora y Faltas de empleados para poder ir probando la api
-
-nombres = ["Nicolas","Juan Cruz","Ramiro","Agustin","Charly","Pedro","Gustavo","Noelia","Catalina"]
-
-valorxhora = [3000,3200,2150,1500,4000,1000,1500,2500,2550]
-
-faltas = [2,0,1,0,0,0,3,1,0]
-
-empleados = []
-for n,v,f in zip(nombres,valorxhora,faltas):
-    empleados.append({
-                        "Nombre":n,
-                        "Paga por hora":v,
-                        "Faltas":f
-                    })
-
-""""
-        Esto Crea una lista de diccionarios del tipo:
-        {"Nombre":...,"Paga por hora":...,"Faltas"}
-"""
-
 # Creo una funcion que al utilizarla pueda usar todas las funcionalidades creadas para mi app
 
 def api(empleados:List[int]):
